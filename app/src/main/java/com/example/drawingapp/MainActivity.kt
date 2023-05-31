7d03345c7509ec171f1f11233aa2e82f9dbd03b9
Alberto Rodrigues <allio.professional@gmail.com>
2023-05-30 22:38:57 -0300

    fazendo mecânica de mudar tamanho do pincel, utilizando um display

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.drawingapp
 
+import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +17,33 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setBrushSize(7f)
+
+        val brushPicker: ImageButton = findViewById(R.id.btn_brushSizePicker)
+        brushPicker.setOnClickListener{
+            displayDialog()
+        }
     }
 
-    private fun showBrushSizePicker(){
+    fun displayDialog(){
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
-        brushDialog.setTitle("Brush size: ")
+        //todo: tentar utilizar "when", para ver se fica mais "bonito"
+        val smallbtn = brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
+        smallbtn.setOnClickListener{
+            drawingView?.setBrushSize(3f)
+            brushDialog.dismiss()
+        }
+        val mediumbtn = brushDialog.findViewById<ImageButton>(R.id.medium_brush)
+        mediumbtn.setOnClickListener{
+            drawingView?.setBrushSize(15f)
+            brushDialog.dismiss()
+        }
+        val extrabtn = brushDialog.findViewById<ImageButton>(R.id.extra_brush)
+        extrabtn.setOnClickListener{
+            drawingView?.setBrushSize(30f)
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
+
 }
